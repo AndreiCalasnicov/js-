4577705e4ad38f562bd0c3a78b64e3fd9892a6a0
AndreiCalasnicov <vselanaia@gmail.com>
2025-05-01 07:39:34 +0300

    Add files via upload

---
+++ list.js
@@ -0,0 +1,21 @@
+let reviews = [
+   { body:"I liked it very much !",
+    author: "jonh doue",
+    rating:"3.5",
+    isReal:true
+   },  
+   { 
+    body:"I liked it very much !",
+    author: "Mary P",
+    rating:"2",
+    isReal:true
+   },  
+   { 
+    body:"bad food....(",
+    author: "Pete K.",
+    rating:"4.5",
+    isReal:false
+   }
+
+
+]
